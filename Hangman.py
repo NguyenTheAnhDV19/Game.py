99a803409865a73a3e7b59b4a48d1d3d189260b0
BoyAnh <90632032+NguyenTheAnhDV19@users.noreply.github.com>
2022-10-10 19:46:47 +0700

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,114 @@
+import pygame 
+import math
+
+pygame.init()
+WIDTH,HIGH= 800,500 
+win =pygame.display.set_mode((WIDTH,HIGH))
+pygame.display.set_caption("Hang man game")
+
+FPS = 60 
+clock = pygame.time.Clock()
+
+
+images = []
+image0 = pygame.image.load(r'C:\T\bmages\bangman0.png')
+images.append(image0)
+image1 = pygame.image.load(r'C:\T\bmages\bangman1.png')
+images.append(image1)
+image2 = pygame.image.load(r'C:\T\bmages\bangman2.png')
+images.append(image2)
+image3 = pygame.image.load(r'C:\T\bmages\bangman3.png')
+images.append(image3)
+image4 = pygame.image.load(r'C:\T\bmages\bangman4.png')
+images.append(image4)
+image5 = pygame.image.load(r'C:\T\bmages\bangman5.png')
+images.append(image5)
+image6 = pygame.image.load(r'C:\T\bmages\bangman6.png')
+images.append(image6)
+
+hangman_status = 0
+
+radius = 20 
+gap = 15 
+
+start_x = round((WIDTH - ((radius*2 + gap) * 13))/2 )
+start_y = 400
+
+A=65
+
+letters = []
+for i in range(26) :
+    x = start_x + gap*2 + ((radius*2 + gap) * (i %13))
+    y = start_y + ((i//13) * (gap + radius*2))
+    letters.append([x,y,chr(A+i),True])
+
+font_letter = pygame.font.SysFont('comicsans',40)
+
+word = "INCREASING"
+guessed = []
+
+def draw() :
+    win.fill((255,255,255))
+    show_up = ""
+
+    for letter in word :
+        if letter in guessed :
+            show_up += letter +" "
+        else :
+            show_up += "_ "
+
+    for letter in letters :
+        x,y, ltr,visible = letter 
+        if visible :
+            pygame.draw.circle(win,(41,41,41),(x,y), radius,3)
+            text = font_letter.render(ltr,1,(41,41,41))
+            win.blit(text,(x - text.get_width()/2,y - text.get_height()/2))
+    
+
+
+    win.blit(images[hangman_status],(150,100))
+    show = font_letter.render(show_up,1,(41,41,41))
+    win.blit(show,(400,200))
+    pygame.display.update()
+
+def display_message(message) :
+    win.fill((255,255,255))
+    text = font_letter.render(message,1,(41,41,41))
+    win.blit(text, (WIDTH/2 - text.get_width()/2, HIGH/2 - text.get_height()/2))
+    pygame.display.update()
+
+run = True 
+while run :
+    clock.tick(FPS)
+    draw()
+
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
+            run = False 
+        if event.type == pygame.MOUSEBUTTONDOWN :
+            m_x,m_y= pygame.mouse.get_pos()
+            for letter in letters :
+                x,y,ltr,visible = letter
+                if visible :
+                    dis = math.sqrt((x - m_x)**2 + (y-m_y)**2)
+                    if dis < radius :
+                        guessed.append(ltr)
+                        letter[3] = False
+                        if ltr not in word :
+                            hangman_status += 1
+
+    won = True 
+    for letter in word :
+        if letter not in guessed :
+            won = False 
+            break
+
+    if won :
+        display_message("U won")
+
+    if hangman_status == 6 :
+        display_message("U lost")
+
+pygame.quit() 
+
+
---
+++ TicTocToe.py
@@ -0,0 +1,162 @@
+import pygame, sys
+import numpy as np
+from pygame.constants import K_0 
+
+pygame.init()
+
+#Colour : (Red, Green, White)
+Grey = (100,100,100)
+Red = (255,0, 0)
+Green=(0,255,0)
+White = (0,0,255)
+
+# width and height of the board :
+width = 500 
+height = 500 
+
+# cordinate :
+cordinate1 = 100
+cordinate2 = 200
+cordinate3 = 300 
+cordinate4 = 400
+#Function to draw a board :
+display = pygame.display.set_mode((width, height))
+pygame.display.set_caption("GAME TIC TOC TOE")
+display.fill(Grey)
+
+#Function to draw a line :
+def draw_line() :
+   # The first vertical line :
+   pygame.draw.line(display,Green,(0, cordinate1),(500, cordinate1), 10)
+   # The second vertical line :
+   pygame.draw.line(display,Green,( 0,cordinate2),(500,cordinate2), 10)
+   #The third vertical line :
+   pygame.draw.line(display,Green,( 0,cordinate3),(500,cordinate3), 10)
+   #The forth vertical line :
+   pygame.draw.line(display,Green,( 0,cordinate4),(500,cordinate4), 10)
+
+   # The first horizental line :
+   pygame.draw.line(display,Green,(cordinate1, 0),(cordinate1, 500), 10)
+
+   pygame.draw.line(display,Green,( cordinate2, 0),(cordinate2, 500), 10)
+
+   pygame.draw.line(display,Green,(cordinate3, 0),(cordinate3, 500), 10)
+
+   pygame.draw.line(display,Green,(cordinate4, 0),(cordinate4, 500), 10)
+
+#Set value for row and col in board :
+row_len = 5 
+col_len = 5 
+
+
+# Set matrix fill with 0 for board :
+board = np.full((row_len, col_len), 0)
+
+#function to mark the square :
+def mark_square(row,col,play) :
+    board[row][col] = play 
+
+def is_square_available(row,col) :
+    return board[row][col] ==0 
+
+def draw_fingure() :
+    for row in range(row_len) :
+        for col in range(col_len) :
+            if board[row][col] == 1 :
+                pygame.draw.circle(display,Red,(int(row*100 + 50),int(col*100 +50)),30,10)
+            elif board[row][col] == 2:
+                pygame.draw.line(display,White,(row* 100+ 30,col*100 +30),(row*100 +100 -30,col* 100 +100 -30 ), 10)
+                pygame.draw.line(display,White,(row* 100+ 30,col *100 +100 -30),(row * 100 +100 - 30, col * 100 +30 ), 10)
+def draw_vertical_line(row,play) :
+    if play == 1 :
+        colour = Red 
+    elif play == 2 :
+        colour = White 
+    pygame.draw.line(display,colour,(row*100+ 50,50),(row*100 +50,500- 50),10)
+
+def draw_horizontial_line(col,play) :
+    if play == 1 :
+        colour = Red 
+    elif play == 2 :
+        colour = White 
+    pygame.draw.line(display,colour,(50,col * 100 + 50),(500- 50,col * 100 +50),10)
+
+def draw_diagnol_line1() :
+    if play == 1 :
+        colour = Red 
+    elif play == 2 :
+        colour = White
+    pygame.draw.line(display,colour,(50,50),(500 - 50, 500 - 50), 10)
+    
+def draw_diagnol_line2() :
+    if play == 1 :
+        colour = Red 
+    elif play == 2 :
+        colour = White 
+    pygame.draw.line(display, colour,(50, 500 - 50),(500 - 50, 50), 10)
+
+def check_win() :
+    for row in range (row_len) :
+        if board[row][0] == play and board[row][1] == play and board[row][2] == play and board[row][3] == play and board[row][4] == play :
+            draw_vertical_line(row,play)
+            return True 
+    for col in range(col_len) :
+        if board[0][col] == play and board[1][col] == play and board[2][col] == play and board[3][col] == play and board[4][col] == play :
+            draw_horizontial_line(col,play)
+            return True 
+    if board[0][0] == play and board[1][1] == play and board[2][2] == play and board[3][3] == play and board[4][4] == play :
+            draw_diagnol_line1()
+            return True 
+    if board[0][4] == play and board[1][3] == play and board[2][2] == play and board[3][1] == play and board[0][4] == play :
+            draw_diagnol_line2()
+            return True 
+
+def restart() :
+   display = pygame.display.set_mode((width, height))
+   pygame.display.set_caption("GAME TIC TOC TOE")
+   display.fill(Grey)
+   draw_line()
+   for row in range(row_len) :
+      for col in range(col_len) :
+         board[row][col] = 0 
+
+play = 1 
+end_game = False
+
+
+while True :
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
+            sys.exit()
+        draw_line()
+        
+        if event.type == pygame.MOUSEBUTTONDOWN and not end_game :
+            X = event.pos[0]
+            Y = event.pos[1]
+        
+            click_row = int(X // 100)
+            click_col = int(Y // 100)
+
+            if is_square_available(click_row, click_col) :
+                if play == 1 :
+                    mark_square(click_row,click_col , 1 )
+                    if check_win() :
+                       end_game = True 
+                    
+                    play = 2 
+            
+                elif play == 2 :
+                    mark_square(click_row,click_col, 2)
+                    if check_win() :
+                       end_game = True 
+                    
+                    play = 1 
+                draw_fingure()
+
+        if event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_SPACE or event.key == pygame.K_BACKSPACE :
+               restart()
+               play = 1 
+               end_game = False
+    
+    pygame.display.update()
